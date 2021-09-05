b239f3d46def1a0f35fa0fc039e72a9953d41bd9
akza07 <itsakza07@gmail.com>
2021-09-05 17:03:29 +0530

    Fixing CORSMiddleware

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from typing import List, Optional
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from .schemas import Symptoms, TokenData
 from sqlalchemy.orm import Session
 from . import services, schemas, models
@@ -15,6 +17,13 @@ from jose import JWTError,jwt
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "token")
 
 # dependency
